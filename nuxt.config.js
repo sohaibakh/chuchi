67de744bd6af58bfe76043235ea865acb23d792f
Kraken0403 <trivedibhavya1997@gmail.com>
2025-03-14 04:46:42 +0530

    fifth

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,8 @@ export default {
      ** Nuxt target
      ** See https://nuxtjs.org/api/configuration-target
      */
-    target: 'server',
+    // target: 'server',
+    target: 'static',
     /*
      ** Headers of the page
      ** See https://nuxtjs.org/api/configuration-head
